93630eb5f6a0138b31dda6ba2e557e67ed2c8aac
Abhishek Acharya <82573224+AbhiWiz-gif@users.noreply.github.com>
2024-04-24 21:42:15 +1000

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,41 +1,54 @@
 pipeline {
     agent any
-    stages{
-        stage('Build'){
-            steps{
+    stages {
+        stage('Build') {
+            steps {
                 echo 'Builded the code using Maven'
             }
         }
-        stage('Unit and Integration Tests'){
-            steps{
+        stage('Unit and Integration Tests') {
+            steps {
                 echo 'Done Unit and integration testing using mvn test'
             }
+            post {
+                success {
+                    emailext subject: 'Pipeline Success',
+        body: 'Your pipeline has succeeded. See attached logs for details.',
+        to: 'abhii.mailboxx@gmail.com',
+        attachLog: true
+                }
+                failure {
+                    emailext subject: 'Pipeline Failure',
+        body: 'Your pipeline has failed. See attached logs for details.',
+        to: 'abhii.mailboxx@gmail.com',
+        attachLog: true
+                }
+            }
         }
-        stage('Code Analysis'){
-            steps{
+        stage('Code Analysis') {
+            steps {
                 echo 'Use SonarQube for code analysis'
             }
         }
-        stage('Security Scan'){
-            steps{
+        stage('Security Scan') {
+            steps {
                 echo 'Use OWASP ZAP for security Scan'
             }
-        }  
-        stage('Deploy to staging'){
-            steps{
+        }
+        stage('Deploy to staging') {
+            steps {
                 echo 'Using AWS CLI to deploy to EC2'
             }
         }
-        stage('Integration tests on staging'){
-            steps{
+        stage('Integration tests on staging') {
+            steps {
                 echo 'Using a tool like Selenium for integration tests'
             }
         }
-        stage('deploy to production'){
-            steps{
+        stage('deploy to production') {
+            steps {
                 echo 'Using AWS CLI to deploy'
             }
         }
     }
-
 }
